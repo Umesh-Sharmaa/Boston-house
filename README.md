48c655a80d4b3dd7e2bbb26b8f78928cf9fbd1d0
Umesh Sharma <us302012@gmail.com>
2024-05-19 13:48:41 +0530

    This is for web app

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Boston House prediction
+# Boston House prediction
+
+## Software and tools require
+
+1. [GitHub Account](https://github.com)
+
+2. [VS Code IDE](https://code.visualstudio.com/)
+
+3. [Heroku](https://heroku.com)
+
+4. [Git CLI](https://git-scm.com/downloads)
+
+Create a new Environment
+
+```
+conda create -p venv python==3.11 -y
+```
---
+++ app.py
@@ -0,0 +1,26 @@
+import pickle 
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+
+app=Flask(__name__)
+
+#Model loading
+
+model=pickle.load(open("house.pkl","rb"))
+scaler=pickle.load(open("scaler.pkl","rb"))
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/predict_api",methods=["POST"])
+def predict_api():
+    data=request.json["data"]
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=model.predict(new_data)
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
